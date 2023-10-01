3a4e7dda1be2bd470c360aba433bd4921ece16a4
Chayenne <chagasdecastro@outlook.com>
2023-10-01 11:16:20 -0300

    dark mode

--- script.js
+++ script.js
@@ -97,37 +97,49 @@ document.addEventListener('DOMContentLoaded', function() {
     const navElement = document.getElementById("meuNav");
     const backgroundElement = document.getElementById("meuBackground");
     const sections = document.querySelectorAll("section");
-    const headerTexts = document.querySelectorAll("#header h1, #header p"); // Seleciona os textos do header
-
-    let coresAlternadas = false;
-
-    // Adicione um ouvinte de clique ao elemento SVG
-    svgElement.addEventListener("click", function() {
-        // Verifique o estado atual das cores e alterne
-        if (coresAlternadas) {
-            navElement.style.backgroundColor = "#221722";
-            backgroundElement.style.backgroundColor = "#37283a";
-            sections.forEach(function(section) {
-                section.style.backgroundColor = "#a1a1a1";
-            });
-            // Altere a cor dos textos do header para branco
-            headerTexts.forEach(function(element) {
-                element.style.color = "#fff";
-            });
-        } else {
+    const headerTexts = document.querySelectorAll("#header h1, #header p");
+    
+    let modoClaro = true; // Inicialmente, o modo claro está ativado
+    
+        // SVG para o modo claro
+        const iconeModoClaro = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" style="cursor: pointer;" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                <path id="iconePathClaro" stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+            </svg>
+        `;
+        
+        // SVG para o modo escuro
+        const iconeModoEscuro = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" style="cursor: pointer;" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" class="w-10 h-10">
+            <path id="iconePathEscuro" stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+        </svg>
+        `;
+    
+    svgElement.addEventListener("click", function () {
+        modoClaro = !modoClaro;
+    
+        // Atualize o ícone com base no estado atual
+        if (modoClaro) {
+            svgElement.innerHTML = iconeModoClaro;
             navElement.style.backgroundColor = "#716075";
             backgroundElement.style.backgroundColor = "#e7e7e7";
-            sections.forEach(function(section) {
+            sections.forEach(function (section) {
                 section.style.backgroundColor = "#fff";
             });
-            // Reverta a cor dos textos do header para sua cor original
-            headerTexts.forEach(function(element) {
-                element.style.color = ""; // Isso removerá qualquer estilo de cor definido, revertendo para a cor original do CSS
+            headerTexts.forEach(function (element) {
+                element.style.color = "";
+            });
+        } else {
+            svgElement.innerHTML = iconeModoEscuro;
+            navElement.style.backgroundColor = "#221722";
+            backgroundElement.style.backgroundColor = "#37283a";
+            sections.forEach(function (section) {
+                section.style.backgroundColor = "#a1a1a1";
+            });
+            headerTexts.forEach(function (element) {
+                element.style.color = "#fff";
             });
         }
-
-    // Alterne o estado das cores
-    coresAlternadas = !coresAlternadas;
-});
+    });   
 
 });
